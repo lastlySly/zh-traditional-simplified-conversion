e72938c8624de04a599d0fc1a75ced241369b409
“lastlySly <lastlySly@163.com>
2019-09-20 10:59:44 +0800

    添加文件夹过滤，文件过滤。补充图片只修改文件名并复制

--- src/main/java/com/lastlysly/utils/MyFileUtils.java
+++ src/main/java/com/lastlysly/utils/MyFileUtils.java
@@ -1,6 +1,10 @@
 package com.lastlysly.utils;
 
+import javax.imageio.ImageIO;
+import java.awt.*;
 import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -9,6 +13,55 @@ import java.util.List;
  * @create 2019-09-16 15:43
  **/
 public class MyFileUtils {
+
+    public static List<String> folderNameList = new ArrayList<>();
+    public static List<String> fileSuffixList = new ArrayList<>();
+
+    static {
+        folderNameList.add(".git");
+        folderNameList.add(".svn");
+        folderNameList.add(".idea");
+//        fileSuffixList.add("iml");
+//        fileSuffixList.add("css");
+//        fileSuffixList.add("eot");
+//        fileSuffixList.add("woff");
+//        fileSuffixList.add("mp3");
+//        fileSuffixList.add("ico");
+//        fileSuffixList.add("ttf");
+
+//
+        fileSuffixList.add("java");
+        fileSuffixList.add("js");
+        fileSuffixList.add("vue");
+        fileSuffixList.add("html");
+        fileSuffixList.add("htm");
+        fileSuffixList.add("jsp");
+        fileSuffixList.add("xml");
+        fileSuffixList.add("pom");
+        fileSuffixList.add("properties");
+        fileSuffixList.add("txt");
+        fileSuffixList.add("md");
+//        fileSuffixList.add();
+
+//        fileSuffixList.add("png");
+//        fileSuffixList.add("jif");
+//        fileSuffixList.add("jpg");
+    }
+
+    /**
+     * 判断是否为图片
+     * @param file
+     * @return
+     */
+    public static boolean isImg(File file){
+        try {
+            Image image = ImageIO.read(file);
+            return image != null;
+        } catch(IOException ex) {
+            return false;
+        }
+    }
+
     /**
      * 获取某路径下所有文件路径
      * @param path
@@ -18,22 +71,67 @@ public class MyFileUtils {
         File file = new File(path);
         // 如果这个路径是文件夹
         if (file.isDirectory()) {
-            // 获取路径下的所有文件
-            File[] files = file.listFiles();
-            for (int i = 0; i < files.length; i++) {
-                // 如果还是文件夹 递归获取里面的文件 文件夹
-                if (files[i].isDirectory()) {
-//                    System.out.println("目录：" + files[i].getPath());
-                    getFilesPathList(files[i].getPath(),filePathList);
-                } else {
-                    filePathList.add(files[i].getPath());
-//                    System.out.println("文件：" + files[i].getPath());
-                }
 
+            if (!folderNameList.contains(file.getName())){
+                // 获取路径下的所有文件
+                File[] files = file.listFiles();
+                for (int i = 0; i < files.length; i++) {
+                    // 如果还是文件夹 递归获取里面的文件 文件夹
+                    if (files[i].isDirectory()) {
+                        getFilesPathList(files[i].getPath(),filePathList);
+                    } else {
+                        String suffix = files[i].getName().substring(files[i].getName().lastIndexOf(".") + 1);
+//                        if (!fileSuffixList.contains(suffix) && !MyFileUtils.isImg(files[i])){
+//                            filePathList.add(files[i].getPath());
+//                        }
+                        if (fileSuffixList.contains(suffix.toLowerCase())){
+                            filePathList.add(files[i].getPath());
+                        }
+                    }
+
+                }
             }
+
         } else {
-            filePathList.add(file.getPath());
-//            System.out.println("文件：" + file.getPath());
+            String suffix = file.getName().substring(file.getName().lastIndexOf(".") + 1);
+            if (fileSuffixList.contains(suffix.toLowerCase())){
+                filePathList.add(file.getPath());
+            }
         }
     }
+
+
+    /**
+     * 获取某路径下所有图片文件路径
+     * @param path
+     * @return
+     */
+    public static void getImgFilesPathList(String path, List<String> imgFilePathList) {
+        File file = new File(path);
+        // 如果这个路径是文件夹
+        if (file.isDirectory()) {
+
+            if (!folderNameList.contains(file.getName())){
+                // 获取路径下的所有文件
+                File[] files = file.listFiles();
+                for (int i = 0; i < files.length; i++) {
+                    // 如果还是文件夹 递归获取里面的文件 文件夹
+                    if (files[i].isDirectory()) {
+                        getImgFilesPathList(files[i].getPath(),imgFilePathList);
+                    } else {
+                        //如果是图片
+                        if (MyFileUtils.isImg(files[i])){
+                            imgFilePathList.add(files[i].getPath());
+                        }
+                    }
+                }
+            }
+        } else {
+            if (MyFileUtils.isImg(file)){
+                imgFilePathList.add(file.getPath());
+            }
+        }
+    }
+
+
 }
--- src/main/java/com/lastlysly/utils/MyUtils.java
+++ src/main/java/com/lastlysly/utils/MyUtils.java
@@ -26,6 +26,7 @@ public class MyUtils {
     public static void scanFolderAndConver(MyZHConverterUtils myZHConverterUtils, String filePath, String converType, JTextArea jTextArea) throws IOException {
         List<String> pathList = new ArrayList<String>(16);
         MyFileUtils.getFilesPathList(filePath,pathList);
+        jTextArea.append("非图片文件转换开始：\n");
         for (String path : pathList){
             jTextArea.append("开始转换："+path + "\n");
             System.out.println(path);
@@ -44,6 +45,46 @@ public class MyUtils {
             jTextArea.append("转换完成："+path + "\n");
             jTextArea.setCaretPosition(jTextArea.getText().length());
         }
+        jTextArea.append("非图片文件转换结束：\n");
+        jTextArea.append("==========================================================================：\n");
+        jTextArea.append("==========================================================================：\n");
+
+
+        jTextArea.append("图片文件文件名转换，并复制开始：\n");
+        List<String> imgPathList = new ArrayList<String>(16);
+        MyFileUtils.getImgFilesPathList(filePath,imgPathList);
+        for(String path : imgPathList){
+            System.out.println(path);
+            File imgFile = new File(path);
+            String imgFileName = imgFile.getName();
+            String newImgFileName = "";
+            if (converType.equals(CommonConstants.zhSimple2zhTw)){
+                newImgFileName = myZHConverterUtils.myConvertToTW(imgFileName);
+                newImgFileName= ZhConverterUtil.convertToTraditional(newImgFileName);
+                //如果转换后图片名不同，则复制这张图片并改名为新名称
+                if (!imgFileName.equals(newImgFileName)){
+                    String newFilePath = imgFile.getParent() + "/" + newImgFileName;
+                    FileUtils.copyFile(imgFile,new File(newFilePath));
+                    System.out.println("新图片："+newFilePath);
+                    jTextArea.append("图片复制完成："+path + "\n");
+                }
+            }else{
+                newImgFileName = myZHConverterUtils.myConvertToSimple(imgFileName);
+                newImgFileName= ZhConverterUtil.convertToSimple(newImgFileName);
+                //如果转换后图片名不同，则复制这张图片并改名为新名称
+                if (!imgFileName.equals(newImgFileName)){
+                    String newFilePath = imgFile.getParent() + "/" + newImgFileName;
+                    FileUtils.copyFile(imgFile,new File(newFilePath));
+                    System.out.println("新图片："+newFilePath);
+                    jTextArea.append("图片复制完成："+path + "\n");
+                }
+            }
+
+            jTextArea.append("图片操作完成："+path + "\n");
+            jTextArea.setCaretPosition(jTextArea.getText().length());
+        }
+
+        jTextArea.append("图片文件文件名转换，并复制结束：\n");
     }
 
 }
